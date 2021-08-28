6ecaaa5e3de7f27572ede4728672bba6defda82e
Martin Faucheux <martin.faucheux@gmail.com>
2021-08-28 17:09:14 +0700

    add music toggle in mobile

--- Assets/Scripts/UI/MusicToggle.cs
+++ Assets/Scripts/UI/MusicToggle.cs
@@ -9,39 +9,46 @@ public class MusicToggle : MonoBehaviour
     [SerializeField] Image crossImage;
     [SerializeField] TextMeshProUGUI onOffText;
     [SerializeField] string soundName = "Game Music";
-    
+
 
     private bool _isEnabled = true;
 
-    private Sound music{
-        get{ return AudioManager.instance.Get(soundName); }
+    private Sound music
+    {
+        get { return AudioManager.instance.Get(soundName); }
     }
 
 
-    public void Toggle(){
-        if(_isEnabled){
+    public void Toggle()
+    {
+        if (_isEnabled)
+        {
             Disable();
         }
-        else{
+        else
+        {
             Enable();
         }
     }
 
-    private void Enable(){
+    private void Enable()
+    {
         music.Unmute();
         SetText("On");
         crossImage.enabled = false;
         _isEnabled = true;
     }
 
-    private void Disable(){
+    private void Disable()
+    {
         music.Mute();
         SetText("Off");
         crossImage.enabled = true;
         _isEnabled = false;
     }
 
-    private void SetText(string text){
-        onOffText.SetText(text);
+    private void SetText(string text)
+    {
+        onOffText?.SetText(text);
     }
 }
