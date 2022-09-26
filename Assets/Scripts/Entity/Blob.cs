10d086edded555c741894527d35d1d5594d5f979
Martin Faucheux <martin.faucheux@gmail.com>
2022-09-26 09:15:13 +0200

    refactor blob code

--- Assets/Scripts/Entity/Blob.cs
+++ Assets/Scripts/Entity/Blob.cs
@@ -6,8 +6,8 @@ public class Blob : MonoBehaviour
 {
 
     [SerializeField] bool isControlled = false;
-    public Transform guyPoolTransform; 
-    public List<Guy> guys {get; private set;}
+    public Transform guyPoolTransform;
+    public List<Guy> guys { get; private set; }
 
     void Start()
     {
@@ -15,16 +15,15 @@ public class Blob : MonoBehaviour
         guys = new List<Guy>();
 
         Guy[] initGuys = guyPoolTransform.GetComponentsInChildren<Guy>();
-        foreach(Guy guy in initGuys){
+        foreach (Guy guy in initGuys)
             Absorb(guy);
-        }
 
-        if (isControlled){
+        if (isControlled)
             TurnManager.instance.Register(this);
-        }
     }
 
-    public (Vector2Int, List<(Entity, Entity)>) GetMovement(Direction direction){
+    public (Vector2Int, List<(Entity, Entity)>) GetMovement(Direction direction)
+    {
         // return the maximum possible displacement as well the list of collided objects
         int distance = 0;
         Vector2Int dirVect = direction.ToPos();
@@ -34,26 +33,31 @@ public class Blob : MonoBehaviour
         bool isDisplacementPossible = true; // main condition to get out of loop
         bool isDisplacementStopped = false; // if a entity pins down the blob
 
-        if(!guys.Any()){
+        if (!guys.Any())
+        {
             // this happens if the blob is emptied withing the turn
             isDisplacementPossible = false;
         }
 
-        while(isDisplacementPossible & !isDisplacementStopped){
+        while (isDisplacementPossible & !isDisplacementStopped)
+        {
 
             // store entities collided at this distance iteration
             List<(Entity, Entity)> iterationEntityList = new List<(Entity, Entity)>();
 
-            foreach(Guy guy in guys){                
+            foreach (Guy guy in guys)
+            {
                 Vector2Int positionToCheck = guy.matrixCollider.matrixPosition + (distance + 1) * dirVect;
                 bool isValidPosition = CollisionMatrix.instance.IsValidPosition(positionToCheck);
                 bool isEntityBlocking = false;
 
                 Entity entityComponent = GetEntityAtPosition(positionToCheck);
-                if (entityComponent != null){
+                if (entityComponent != null)
+                {
                     isEntityBlocking = entityComponent.IsBlocking(guy);
 
-                    if (entityComponent.CanInteract(guy)){
+                    if (entityComponent.CanInteract(guy))
+                    {
                         iterationEntityList.Add((guy, entityComponent));
                         isDisplacementStopped = entityComponent.isStopMovement;
                     }
@@ -64,12 +68,12 @@ public class Blob : MonoBehaviour
 
             // trigger Interact method only for entities that have interactWhenOutOfReach
             // in case the movement has been blocked
-            foreach((Entity collidingEntity, Entity collidedEntity) in iterationEntityList){
-                if(isDisplacementPossible | collidedEntity.interactWhenOutOfReach){
+            foreach ((Entity collidingEntity, Entity collidedEntity) in iterationEntityList)
+            {
+                if (isDisplacementPossible | collidedEntity.interactWhenOutOfReach)
                     collidedEntities.Add((collidingEntity, collidedEntity));
-                }
             }
-            
+
             // only add if displacement is possible
             distance += isDisplacementPossible ? 1 : 0;
         }
@@ -77,63 +81,66 @@ public class Blob : MonoBehaviour
         return (maxDisplacement, collidedEntities);
     }
 
-    private static Entity GetEntityAtPosition(Vector2Int matrixPosition){
+    private static Entity GetEntityAtPosition(Vector2Int matrixPosition)
+    {
         // helper for collision resolution
         GameObject objectAtPosition = CollisionMatrix.instance.GetObjectAtPosition(matrixPosition);
-        if (objectAtPosition != null){
+        if (objectAtPosition != null)
+        {
             Entity entity = objectAtPosition.GetComponent<Entity>();
-            if (entity != null){    
+            if (entity != null)
                 return entity;
-            }
         }
         return null;
     }
 
-    public void Absorb(Guy guy){
+    public void Absorb(Guy guy)
+    {
         guy.Extract(); // remove the guy from his current blob
         guys.Add(guy);
         guy.blob = this;
         guy.transform.SetParent(guyPoolTransform);
     }
 
-    public void Absorb(Blob absorbedBlob){
+    public void Absorb(Blob absorbedBlob)
+    {
         // mark absorbed blob as not controlled
         TurnManager.instance.Unregister(absorbedBlob);
         absorbedBlob.isControlled = false;
 
         // absorb remaining guys
         // use a copy of the list because it will be modified
-        foreach(Guy guy in new List<Guy>(absorbedBlob.guys)){
+        foreach (Guy guy in new List<Guy>(absorbedBlob.guys))
             Absorb(guy);
-        }
     }
 
-    public void Amaze(){
-        foreach(Guy guy in guys){
+    public void Amaze()
+    {
+        foreach (Guy guy in guys)
             guy.Amaze();
-        }
     }
 
-    public void Remove(Guy guy){
-        guys.Remove(guy);
-    }
+    public void Remove(Guy guy) => guys.Remove(guy);
 
-    public int GetMovementPriority(Direction direction){
+    public int GetMovementPriority(Direction direction)
+    {
         int minDistance = CollisionMatrix.instance.maxDistance;
-        foreach(Guy guy in guys){
+        foreach (Guy guy in guys)
+        {
             int distance = GetDistanceToBorder(guy.matrixPosition, direction);
-            if(distance < minDistance){
+            if (distance < minDistance)
                 minDistance = distance;
-            }
         }
         return minDistance;
     }
 
-    private static int GetDistanceToBorder(Vector2Int matrixPosition, Direction direction){
+    private static int GetDistanceToBorder(Vector2Int matrixPosition, Direction direction)
+    {
         int result = 0;
         Vector2Int maxPos = CollisionMatrix.instance.matrixSize;
         Vector2Int dirVect = direction.ToPos();
-        switch(direction.ToString()){
+        switch (direction.ToString())
+        {
             case "UP":
                 result = maxPos.y - matrixPosition.y;
                 break;
@@ -147,9 +154,8 @@ public class Blob : MonoBehaviour
                 result = maxPos.x - matrixPosition.x;
                 break;
             default:
-                break;       
+                break;
         }
-
         return result;
     }
 }
