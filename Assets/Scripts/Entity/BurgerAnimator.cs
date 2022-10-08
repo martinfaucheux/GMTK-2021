f43dd424228a9fb040b63bd43e110fbb63b442fc
Martin Faucheux <martin.faucheux@gmail.com>
2022-10-08 14:29:44 +0200

    improve burger burn animation

--- Assets/Scripts/Entity/BurgerAnimator.cs
+++ Assets/Scripts/Entity/BurgerAnimator.cs
@@ -8,6 +8,7 @@ public class BurgerAnimator : MonoBehaviour
     [SerializeField] Animator _animator;
     [SerializeField] Color _frozenColor;
     [SerializeField] Color _rottenColor;
+    [SerializeField] ParticleSystem _ashParticles;
     private float _colorChangeTransition
     {
         set { _spriteRenderer.material.SetFloat(materialColorTransitionProperty, value); }
@@ -23,7 +24,8 @@ public class BurgerAnimator : MonoBehaviour
     private static string materialColorTransitionProperty = "_Transition";
     private static string materialShiftColorProperty = "_ShiftColor";
     private static string materialRotTransitionProperty = "_Melt";
-    private static float _transitionTime = 0.2f;
+    private static float _colorTransitionDuration = 0.2f;
+    private static float _fadeAwayDuration = 3f;
     private static float _meltMaxValue = 0.2f;
     public void SetFrozen()
     {
@@ -40,14 +42,14 @@ public class BurgerAnimator : MonoBehaviour
             t => _colorChangeTransition = t,
             0f,
             1f,
-            _transitionTime
+            _colorTransitionDuration
         );
         LeanTween.value(
             gameObject,
             t => _meltTransition = t,
             0f,
             _meltMaxValue,
-            _transitionTime
+            _colorTransitionDuration
         );
 
         _animator.SetBool("frozen", true);
@@ -61,15 +63,27 @@ public class BurgerAnimator : MonoBehaviour
             t => _colorChangeTransition = t,
             1f,
             0f,
-            _transitionTime
+            _colorTransitionDuration
         );
         _animator.SetBool("frozen", false);
     }
 
     public void SetBurned()
     {
-        // The burger juste got burned
-        LeanTween.color(gameObject, Color.black, _transitionTime);
         _animator.SetTrigger("burn");
+        // Vector3 targetSize = transform.localScale;
+        // targetSize.y = 0f;
+        // targetSize.x *= 1.3f;
+        // LeanTween.scale(gameObject, targetSize, _colorTransitionDuration + _fadeAwayDuration);
+
+        LTSeq sequence = LeanTween.sequence();
+        sequence.append(
+            LeanTween.color(gameObject, Color.black, _colorTransitionDuration)
+        );
+        sequence.append(2f);
+        sequence.append(() => _ashParticles?.Play());
+        sequence.append(
+            LeanTween.alpha(gameObject, 0f, _fadeAwayDuration).setEaseInQuint()
+        );
     }
 }
