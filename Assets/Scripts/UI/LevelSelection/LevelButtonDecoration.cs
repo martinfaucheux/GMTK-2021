38b61418572ed514d4db9169f5de5ac6b8922981
Martin Faucheux <martin.faucheux@gmail.com>
2022-10-29 12:25:17 +0200

    add predictive random for palm tree disposition

--- Assets/Scripts/UI/LevelSelection/LevelButtonDecoration.cs
+++ Assets/Scripts/UI/LevelSelection/LevelButtonDecoration.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class LevelButtonDecoration : MonoBehaviour
 {
     [SerializeField] float _enableRatio = 0.2f;
+    private static int RANDOM_SEED = 42;
+    private static System.Random _randomizer;
 
     void Start()
     {
         foreach (Transform childTransform in transform)
-        {
-            childTransform.gameObject.SetActive(Random.Range(0f, 1f) < _enableRatio);
-        }
+            childTransform.gameObject.SetActive(GetRandomValue() < _enableRatio);
+    }
+
+    private static float GetRandomValue()
+    {
+        if (_randomizer == null)
+            _randomizer = new System.Random(RANDOM_SEED);
+
+        return (float)_randomizer.NextDouble();
     }
 }
